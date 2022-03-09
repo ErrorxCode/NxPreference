82ee963f1ad09a45fb989e313677e44cf64d188c
Rahil khan <65817230+ErrorxCode@users.noreply.github.com>
2022-03-09 13:31:04 +0530

    Delete EasyPreference directory

--- EasyPreference/src/main/java/com/easypreference/ViewNotSavableException.java
+++
@@ -1,7 +0,0 @@
-package com.easypreference;
-
-public class ViewNotSavableException extends IllegalArgumentException{
-    protected ViewNotSavableException(String id){
-        super("The view " + id + " is not savable. See the list of savable views at documentation https://github.com/ErrorxCode/EasyPreference");
-    }
-}
